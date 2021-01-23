0d444aab77d038e7839792c61b17286843f52442
roadpilot <mdacht777@gmail.com>
2021-01-23 10:42:00 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component {   
+// class App extends Component {   
   
+//   render() {
+//     return (
+//       <div>
+//         <h1>CatBook</h1>
+//         {/* add CatList component here */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
+
+class App extends Component {
+ 
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
+ 
   render() {
+    console.log(this.props.catPics) // log will fire every time App renders
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {/* missing component */}
+        <CatList catPics={this.props.catPics}/>
       </div>
     );
   }
 }
-
-export default App
-
+ 
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,22 @@
 // write and export your action creator function here
+
+// fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+//   return response.json()
+// }).then(responseJSON => {
+//   // instead of logging here, call dispatch and send the cat JSON data to your store
+//   console.log(responseJSON.images)
+// })
+
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+    return response.json()
+    }).then(responseJSON => {
+    // instead of logging here, call dispatch and send the cat JSON data to your store
+    console.log(responseJSON.images)
+    dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+    })
+  };
+}
+
