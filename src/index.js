3730cafcf2bfba6c846c8cb3fbbfa0551cb75fe8
roadpilot <mdacht777@gmail.com>
2021-01-23 10:41:44 -0700

    commit tree 1347fa7bd93236394e9e8f40276a9ad5bcf367c9

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App'
-
-
+import ReactDOM from 'react-dom';
+import App from './App';
+ 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import catsReducer from './reducers/catsReducer.js';
+ 
+const store = createStore(catsReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk),
+)
+ 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
